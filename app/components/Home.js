0e825909def0404546cd2075bbbded268613db77
梁琰沁 <1780304097@qq.com>
2018-03-18 11:08:57 +0800

    update changes

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Home() {
 	return(
 		<div className='home-container'>
-			<h1>Github Battle: Battle your friends and go better together.</h1>
+			<h1>Github Battle: Battle your friends and go better together ; )</h1>
 			<Link className='button' to='/battle'>Battle</Link>
 		</div>
 	)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-var path=require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path=require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack=require('webpack');
 
-module.exports={
+const config={
 	entry:['babel-polyfill','./app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -14,12 +15,25 @@ module.exports={
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
-  devServer:{
-    historyApiFallback:true,
-  },
+  // devServer:{
+  //   historyApiFallback:true,
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
   ]
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports=config;
